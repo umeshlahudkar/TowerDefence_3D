3e83eeb8facd1571cf1989ffcce97019c7c4b317
Umesh Lahudkar <99596032+umeshlahudkar@users.noreply.github.com>
2024-03-05 22:08:35 +0530

    Camera clamp, zoom, move impl

---
+++ TowerDefence_3D/Assets/Script/CameraController.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Transform thisTransform;
+
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float minZoom;
+    [SerializeField] private float maxZoom;
+
+    [SerializeField] private Vector2 minBound;
+    [SerializeField] private Vector2 maxBound;
+
+    private float cameraDistance;
+
+    private float scrollValue;
+    private float horizontal;
+    private float vertical;
+
+    private Vector3 movementVector;
+
+  
+    private void Start()
+    {
+        mainCamera = gameObject.GetComponent<Camera>();
+        thisTransform = gameObject.transform;
+        movementVector = thisTransform.position;
+        cameraDistance = Mathf.Abs(Mathf.Abs(maxBound.y) + Mathf.Abs(minBound.y)) / 2;
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if(horizontal != 0 || vertical != 0)
+        {
+            Move();
+        }
+
+        scrollValue = Input.GetAxisRaw("Mouse ScrollWheel");
+        if(scrollValue != 0)
+        {
+            Zoom(scrollValue);
+            ClampCamera();
+        }
+    }
+
+    private float GetHeight()
+    {
+        if(!mainCamera.orthographic)
+        {
+            return cameraDistance * Mathf.Tan(mainCamera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        }
+
+        return mainCamera.orthographicSize;
+    }
+
+    private void Move()
+    {
+        if(horizontal != 0)
+        {
+            movementVector.x += horizontal * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            movementVector.z += vertical * moveSpeed * Time.deltaTime;
+        }
+
+        ClampCamera();
+    }
+
+    private void ClampCamera()
+    {
+        float height = GetHeight();
+        float width = height * mainCamera.aspect;
+
+        movementVector.x = Mathf.Clamp(movementVector.x, minBound.x + width, maxBound.x - width);
+        movementVector.z = Mathf.Clamp(movementVector.z, minBound.y + height, maxBound.y - height);
+
+        thisTransform.position = movementVector;
+    }
+
+    private void Zoom(float zoomValue)
+    {
+        float newFieldOfView = mainCamera.fieldOfView - (zoomValue * zoomSpeed);
+        newFieldOfView = Mathf.Clamp(newFieldOfView, minZoom, maxZoom);
+        mainCamera.fieldOfView = newFieldOfView;
+    }
+}
